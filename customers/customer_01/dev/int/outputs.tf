799258bf50f09da508b19c073a653f01a39b2f6d
Scott Cheadle <scott.cheadle@sky.uk>
2025-04-14 16:26:33 +0100

    Update INT for single region

--- customers/customer_01/dev/int/outputs.tf
+++ customers/customer_01/dev/int/outputs.tf
@@ -7,13 +7,3 @@ output "main_rds_arn" {
   description = "RDS instance ARN for the main environment"
   value       = module.main_rds.rds_arn
 }
-
-output "dr_rds_endpoint" {
-  description = "RDS instance endpoint for the DR environment"
-  value       = module.dr_rds.rds_endpoint
-}
-
-output "dr_rds_arn" {
-  description = "RDS instance ARN for the DR environment"
-  value       = module.dr_rds.rds_arn
-}
--- customers/customer_01/dev/int/providers.tf
+++ customers/customer_01/dev/int/providers.tf
@@ -1,31 +1,9 @@
 provider "aws" {
   region  = "eu-west-1"
   profile = var.profile
-}
-
-provider "aws" {
-  region  = "eu-west-1"
-  alias   = "main"
-  profile = var.profile
   default_tags {
     tags = {
       region      = "eu-west-1"
-      provisioner = "terraform"
-      account     = "dev"
-      environment = "int"
-      Owner       = "customer1"
-    }
-  }
-
-}
-
-provider "aws" {
-  region  = "eu-west-2"
-  alias   = "dr"
-  profile = var.profile
-  default_tags {
-    tags = {
-      region      = "eu-west-2"
       provisioner = "terraform"
       account     = "dev"
       environment = "int"
